f5eda4c4ae2829ec177f0f80d585445b8f63f292
Gaurav <gsethi924@gmail.com>
2024-03-03 21:14:54 +0530

    final change

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Define variables
 let data = [];
 const Api = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
 const table = document.getElementById('crypto-data');
@@ -7,27 +6,22 @@ const sortByMktCap = document.getElementById('sort1');
 const sortByPercentage = document.getElementById('sort2');
 const loader = document.getElementById('loader'); 
 
-// Function to fetch data
 async function fetchedData() {
     try {
         loader.style.display = "block";
         const response = await fetch(Api);
         data = await response.json();
         showData(data);
-        addEventListeners(); // Add event listeners after data is fetched
+        addEventListeners(); 
     } catch (e) {
         alert(e);
     } finally {
-        loader.style.display = "none"; // Hide loader after data is fetched
+        loader.style.display = "none"; 
     }
 }
 
-// Function to display data in the table
 function showData(data) {
-    // Clear existing table rows
     table.innerHTML = '';
-
-    // Iterate over data and create table rows
     for (let i = 0; i < data.length; i++) {
         const tr = document.createElement('tr');
         const twenty24HrChange = data[i].price_change_percentage_24h;
@@ -44,21 +38,21 @@ function showData(data) {
     }
 }
 
-// Function to add event listeners
+
 function addEventListeners() {
-    // Event listener for sorting by market cap
+
     sortByMktCap.addEventListener('click', () => {
         const sortedByMktCap = data.sort((a, b) => b.market_cap - a.market_cap);
         showData(sortedByMktCap);
     });
 
-    // Event listener for sorting by percentage change
+
     sortByPercentage.addEventListener('click', () => {
         const sortedByPercentage = data.sort((a, b) => b.price_change_percentage_24h - a.price_change_percentage_24h);
         showData(sortedByPercentage);
     });
 
-    // Event listener for search
+    
     search.addEventListener('keyup', () => {
         const searchItem = search.value.toLowerCase();
         const filteredData = data.filter(item => {
@@ -70,5 +64,4 @@ function addEventListeners() {
     });
 }
 
-// Call fetchedData function to fetch and display initial data
 fetchedData();
